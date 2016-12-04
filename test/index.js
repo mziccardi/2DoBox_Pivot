80a050df27acb038d99e00accf3501cf3fb40950
Mike Ziccardi <mikeziccardi@gmail.com>
2016-12-04 14:43:33 -0700

    sets up selenium tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,28 @@
 const assert = require('chai').assert
+const webdriver = require('selenium-webdriver')
+const test = require('selenium-webdriver/testing')
 
-describe('our test bundle', function() {
-  it('should work ⚔️', function () {
-    assert(true)
+test.describe('testing ideabox', function(){
+  this.timeout(10000)
+  test.it('should allow me to add a title and a description', ()=>{
+    const driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+    driver.get('http://localhost:8080');
+
+    const title = driver.findElement({className: 'title-input'})
+    const description = driver.findElement({className: 'body-input'})
+    title.sendKeys('this is a title').then(()=>{
+      return title.getAttribute('value')
+    }).then((value)=>{
+      assert.equal(value, 'this is a title')
+    })
+
+    description.sendKeys('this is a description').then(()=>{
+      return description.getAttribute('value')
+    }).then((value)=>{
+      assert.equal(value, 'this is a description')
+    })
   })
 })
